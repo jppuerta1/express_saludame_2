cf2d0ac81434ec695f06bc19c5cc09ef41678d71
jppuerta1 <jppuertaorrego@gmail.com>
2020-11-11 18:11:52 -0800

    segundo commit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express ()
 
 app.get('/makers/:name',(req,res)=>{
     const name = req.params.name;
-    res.send(`<h1 style="text-transform:capitalize;">${name}</h1>`)
+    res.send(`<h1 style="text-transform:capitalize;">Hola ${name}!</h1>`)
 })
 
 app.listen (3000,()=>console.log('listening on port 3000'))
