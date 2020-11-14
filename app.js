1a8d04fccab15f7f6c90f6fca7282842520cf6e0
jppuerta1 <jppuertaorrego@gmail.com>
2020-11-14 07:26:25 -0800

    cuarto commit

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ const app = express ()
 
 app.get('/makers/:name',(req,res)=>{
     const name = req.params.name;
+    let capName = name.charAt(0).toUpperCase() + name.slice(1).toLowerCase()
 
-    res.send(`<h1>hola ${name.toUpperCase()}!</h1>`)
+    res.send(`<h1>Hola ${capName}!</h1>`)
 })
 
 app.listen (3000,()=>console.log('listening on port 3000'))
